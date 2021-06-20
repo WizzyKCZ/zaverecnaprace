24368577fb3fffa06367b46e3ec66da712140927
WizzyKCZ <wizzy172@seznam.cz>
2021-06-20 21:39:33 +0200

    chybove hlasky

--- games/views.py
+++ games/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from games.models import Hra
 
@@ -44,20 +45,43 @@ class HraDetailView(DetailView):
     template_name = 'game/detail.html'
 
 
-class HraCreate(CreateView):
+class HraCreate(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Hra
     template_name = 'game/hra_bootstrap_form.html'
     form_class = HraModelForm
+    permission_required = 'games.add_hra'
+    login_url = '/accounts/login/'
 
 
-class HraUpdate(UpdateView):
+class HraUpdate(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Hra
     template_name = 'game/hra_bootstrap_form.html'
     form_class = HraModelForm
     # fields = '__all__' # Not recommended (potential security issue if more fields added)
+    permission_required = 'games.change_hra'
+    login_url = '/accounts/login/'
 
 
-class HraDelete(DeleteView):
+class HraDelete(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Hra
     success_url = reverse_lazy('hry')
     template_name = 'hra_confirm_delete.html'
+    login_url = '/accounts/login/'
+    permission_required = 'games.delete_hra'
+
+
+def error_404(request, exception=None):
+    return render(request, 'errors/404.html')
+
+
+def error_500(request):
+    return render(request, 'errors/500.html')
+
+
+def error_403(request, exception=None):
+    return render(request, 'errors/403.html')
+
+
+def error_400(request, exception=None):
+    return render(request, 'errors/400.html')
+
--- zaverecnaprace/urls.py
+++ zaverecnaprace/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import games
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,3 +19,19 @@ urlpatterns = [
 admin.site.site_header = "DDH Administrace"
 admin.site.site_title = "Databáze digitálních her"
 admin.site.index_title = "Vítejte v administrační části DDH"
+
+handler404 = 'games.views.error_404'
+handler500 = 'games.views.error_500'
+handler403 = 'games.views.error_403'
+handler400 = 'games.views.error_400'
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+    urlpatterns += [re_path(r'^500/$', games.views.error_500)]
+    urlpatterns += [re_path(r'^400/$', games.views.error_400)]
+    urlpatterns += [re_path(r'^404/$', games.views.error_404)]
+    urlpatterns += [re_path(r'^403/$', games.views.error_403)]
